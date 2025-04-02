7dba6f905e2648aab80c206f7e463fec06f8eda9
AstridMorgan <146148506+AstridMorgan@users.noreply.github.com>
2025-04-02 04:56:49 -0600

    Add files via upload

---
+++ src/main/java/com/proyectoDesarrollo/repository/AyudaRepository.java
@@ -0,0 +1,8 @@
+package com.proyectoDesarrollo.repository;
+
+import com.proyectoDesarrollo.domain.Ayuda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AyudaRepository extends JpaRepository<Ayuda, Long>{
+    
+}
