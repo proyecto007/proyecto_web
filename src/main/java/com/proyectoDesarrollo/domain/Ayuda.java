92d1d065a045c21296b0822b02b6bb0140ff9601
AstridMorgan <146148506+AstridMorgan@users.noreply.github.com>
2025-04-02 04:55:49 -0600

    Add files via upload

---
+++ src/main/java/com/proyectoDesarrollo/domain/Ayuda.java
@@ -0,0 +1,53 @@
+package com.proyectoDesarrollo.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ayuda") // Asegúrate de que este es el nombre correcto de la tabla en la base de datos
+public class Ayuda implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descripcion;
+    private String contenido;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+}
